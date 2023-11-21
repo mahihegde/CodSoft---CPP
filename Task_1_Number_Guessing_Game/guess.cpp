58a3ee9ca91b6fd5ecc39068e41adc72e3c29946
Mahi Naresh Hegde <144036288+mahihegde@users.noreply.github.com>
2023-11-21 13:29:59 +0530

    Update guess.cpp

--- Task_1_Number_Guessing_Game/guess.cpp
+++ Task_1_Number_Guessing_Game/guess.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int num = rand() % 100 + 1;
+    int userguess;
+    int attempts = 0;
+    cout << "Welcome to Guess the Number Game!" << endl;
+    do {
+        cout << "Enter your guess (between 1 and 100): ";
+        cin >> userguess;
+        
+        if (userguess == num) {
+            cout << "Congratulations! You guessed the right number in " << attempts << " attempts." << endl;
+        } else if (userguess < num) {
+            cout << "That's lower than the number. Try again." << endl;
+        } else {
+            cout << "That's higher than the number. Try again." << endl;
+        }
+        attempts++;
+    } while (userguess != num);
+
+    return 0;
+}
 
